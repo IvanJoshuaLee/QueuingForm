8c80bf0a94b92ee5e88520fb11f1b4e4a9d497e6
IvanJoshua <IvanJoshua@DESKTOP-LEE>
2022-11-18 12:24:35 +0800

    Add project files.

---
+++ QueuingForm/CashierWindowQueueForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueuingForm
+{
+    public partial class CashierWindowQueueForm : Form
+    {
+        CashierClass obj = new CashierClass();
+
+        public CashierWindowQueueForm()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = (1 * 1000);
+            timer.Tick += new EventHandler(Timer1_tick); //timer1_tick represents the name of Tick Event
+            timer.Start();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+        public void DisplayCashierQueue(IEnumerable CashierList)
+
+        {
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+                private void Timer1_tick(object sender, EventArgs e)
+        {
+            btnRefresh.PerformClick();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            CashierClass.CashierQueue.Dequeue();
+        }
+    }
+}
---
+++ QueuingForm/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace QueuingForm
+{
+    partial class QueuingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCashier = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblQueue = new System.Windows.Forms.Label();
+            this.libQueue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCashier
+            // 
+            this.btnCashier.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCashier.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnCashier.Location = new System.Drawing.Point(29, 23);
+            this.btnCashier.Name = "btnCashier";
+            this.btnCashier.Size = new System.Drawing.Size(134, 152);
+            this.btnCashier.TabIndex = 0;
+            this.btnCashier.Text = "Cashier";
+            this.btnCashier.UseVisualStyleBackColor = true;
+            this.btnCashier.Click += new System.EventHandler(this.btnCashier_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(39, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "*Click to get a number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblQueue
+            // 
+            this.lblQueue.AutoSize = true;
+            this.lblQueue.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQueue.Location = new System.Drawing.Point(196, 50);
+            this.lblQueue.Name = "lblQueue";
+            this.lblQueue.Size = new System.Drawing.Size(219, 31);
+            this.lblQueue.TabIndex = 2;
+            this.lblQueue.Text = "Position in Queue";
+            this.lblQueue.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // libQueue
+            // 
+            this.libQueue.AutoSize = true;
+            this.libQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.libQueue.Location = new System.Drawing.Point(202, 97);
+            this.libQueue.Name = "libQueue";
+            this.libQueue.Size = new System.Drawing.Size(307, 61);
+            this.libQueue.TabIndex = 3;
+            this.libQueue.Text = "                    ";
+            // 
+            // QueuingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 216);
+            this.Controls.Add(this.libQueue);
+            this.Controls.Add(this.lblQueue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCashier);
+            this.Name = "QueuingForm";
+            this.Text = "QueuingForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCashier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblQueue;
+        private System.Windows.Forms.Label libQueue;
+    }
+}
+
